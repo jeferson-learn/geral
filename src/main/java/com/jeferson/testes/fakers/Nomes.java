94ff13ffe0b29d8e39e33b269eb7b1305af59afb
jeferson-learn <jefynhuwork@gmail.com>
2022-04-13 12:28:27 -0300

    Modifiquei e melhor direto imprimir

--- src/main/java/com/jeferson/testes/fakers/Nomes.java
+++ src/main/java/com/jeferson/testes/fakers/Nomes.java
@@ -10,26 +10,15 @@ public class Nomes {
     static Faker fakerBr = new Faker(new Locale("pt-BR")); // especifico BRASILEIRO
 
     public static void main(String[] args) {
-
-        nomeCompleto();
-        primeiroNome();
-        ultimoNome();
-
+        System.out.println(fakerBr.name().fullName()); // nome do completo
+        System.out.println(fakerBr.name().firstName()); // primeiro do nome
+        System.out.println(fakerBr.name().lastName()); // ultimo do sobrenome
+        System.out.println(fakerBr.name().name());
+        System.out.println(fakerBr.name().prefix()); // Dra, Sra, Dr, Dra
+        System.out.println(fakerBr.name().suffix()); // parece ultimo sobrenome
+        System.out.println(fakerBr.name().bloodGroup()); // tipos sangue
+        System.out.println(fakerBr.name().nameWithMiddle()); // parece que nome completo tambem
+        System.out.println(fakerBr.name().title()); // nome do titulo qualquer
+        System.out.println(fakerBr.name().username()); // nome do usuario
     }
-
-    private static void ultimoNome() {
-        String ultimoNome = fakerBr.name().lastName();
-        System.out.println(ultimoNome);
-    }
-
-    private static void primeiroNome() {
-        String primeiroNome = fakerBr.name().firstName();
-        System.out.println(primeiroNome);
-    }
-
-    private static void nomeCompleto() {
-        String nomeCompleto = fakerBr.name().fullName();
-        System.out.println(nomeCompleto);
-    }
-
 }
